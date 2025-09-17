e3d6bf1f044aff34e59313c3b9806bb2841c01d2
igaramadana <igrmdns085@gmail.com>
2025-09-17 14:52:55 +0700

    feat: input-validation

--- bin/bmi_kalkulator.dart
+++ bin/bmi_kalkulator.dart
@@ -35,3 +35,15 @@ void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
     );
   }
 }
+
+void main() {
+  List<Map<String, dynamic>> riwayat = [];
+
+  // Simulasi beberapa input
+  hitungBMI(170, 65, riwayat);
+  hitungBMI(165, 75, riwayat);
+  hitungBMI(180, 85, riwayat);
+
+  // Tampilkan riwayat menggunakan perulangan
+  tampilkanRiwayat(riwayat);
+}
