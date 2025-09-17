e61e52b20457d9d5fd679d319d5d2fc77517dbd2
igaramadana <igrmdns085@gmail.com>
2025-09-17 14:51:32 +0700

    feat: history-feature

--- bin/bmi_kalkulator.dart
+++ bin/bmi_kalkulator.dart
@@ -27,3 +27,11 @@ void hitungBMI(
     'kategori': kategori,
   });
 }
+
+void tampilkanRiwayat(List<Map<String, dynamic>> riwayat) {
+  for (var data in riwayat) {
+    print(
+      "Tinggi: ${data['tinggi']} cm, Berat: ${data['berat']} kg, BMI: ${data['bmi'].toStringAsFixed(2)}, Kategori: ${data['kategori']}",
+    );
+  }
+}
